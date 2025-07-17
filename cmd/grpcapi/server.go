947838e84cc82981118d4c83a1fb79886d1e438b
jayson <jayson.santiago1216@gmail.com>
2025-07-17 05:20:41 -0600

    Test PR only branch again

--- cmd/grpcapi/server.go
+++ cmd/grpcapi/server.go
@@ -43,6 +43,8 @@ func createMongoDBClient() {
 	defer func() {
 		if err := client.Disconnect(ctx); err != nil {
 			log.Fatalln("Unable to disconnect to mongodb:", err)
+		} else {
+			log.Println("Disconnected to mongodb")
 		}
 	}()
 

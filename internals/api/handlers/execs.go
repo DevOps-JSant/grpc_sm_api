bd1887f44827f0b28792f9faa0bbe43d09f4df15
jayson <jayson.santiago1216@gmail.com>
2025-07-25 02:12:59 -0600

    Added Get Execs handler, updated pagesize and pagenumber from int32 to uint32 in proto file

--- internals/api/handlers/execs.go
+++ internals/api/handlers/execs.go
@@ -9,6 +9,28 @@ import (
 	pb "jsantdev.com/grpc_sm_api/proto/gen"
 )
 
+func (s *Server) GetExecs(ctx context.Context, req *pb.GetExecsRequest) (*pb.Execs, error) {
+	execFilter := req.GetExec()
+	sortFields := req.GetSortBy()
+
+	pageNumber := req.PageNumber
+	pageSize := req.PageSize
+
+	if pageNumber < 1 {
+		pageNumber = 1
+	}
+
+	if pageSize < 1 {
+		pageSize = 10
+	}
+	execs, err := mongodb.GetExecs(ctx, execFilter, sortFields, pageNumber, pageSize)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &pb.Execs{Execs: execs}, nil
+
+}
 func (s *Server) AddExecs(ctx context.Context, req *pb.Execs) (*pb.Execs, error) {
 
 	execsFromReq := req.GetExecs()
--- internals/repositories/mongodb/execs_crud.go
+++ internals/repositories/mongodb/execs_crud.go
@@ -7,10 +7,59 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 	"jsantdev.com/grpc_sm_api/internals/models"
 	"jsantdev.com/grpc_sm_api/pkg/utils"
 	pb "jsantdev.com/grpc_sm_api/proto/gen"
 )
+
+func GetExecs(ctx context.Context, execFilterFromReq *pb.Exec, sortFieldsFromReq []*pb.SortField, pageNumber, pageSize uint32) ([]*pb.Exec, error) {
+	// Filtering, getting the filters from the request
+	filters, err := utils.BuildFilter(execFilterFromReq, &models.Exec{})
+	if err != nil {
+		return nil, err
+	}
+
+	// Sorting. getting the sort options from the request
+	sortOptions := utils.BuildSortOptions(sortFieldsFromReq)
+
+	// Access the database to fetch data
+	client, err := CreateMongoClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			log.Println("Unable to disconnect to mongodb:", err)
+		}
+	}()
+
+	col := client.Database("school").Collection("execs")
+
+	findOptions := options.Find()
+	findOptions.SetSkip(int64((pageNumber - 1) * pageSize))
+	findOptions.SetLimit(int64(pageSize))
+
+	var cursor *mongo.Cursor
+	if len(sortOptions) > 0 {
+		findOptions.SetSort(sortOptions)
+	}
+
+	cursor, err = col.Find(ctx, filters, findOptions)
+
+	if err != nil {
+		return nil, utils.ErrorHandler(err, "Unable to retrieve data")
+	}
+	defer cursor.Close(ctx)
+
+	execs, err := utils.DecodeEntities(ctx, cursor, func() *pb.Exec { return &pb.Exec{} }, func() *models.Exec { return &models.Exec{} })
+	if err != nil {
+		return nil, err
+	}
+
+	return execs, nil
+}
 
 func AddExecs(ctx context.Context, execsFromReq []*pb.Exec) ([]*pb.Exec, error) {
 	client, err := CreateMongoClient(ctx)
